aee844d5fdcbbc8b3bb39199183044a6fc824611
Reiner Hüchting <dev@reinerhuechting.de>
2023-03-15 16:37:39 +0100

    Add stubs for different sort algorithms.

---
+++ examples/src/main/java/de/reinerh/courses/java/Sort.java
@@ -0,0 +1,31 @@
+package de.reinerh.courses.java;
+
+import java.util.List;
+
+public class Sort {
+
+    /** Implementierung des Sortierverfahrens InsertionSort. */
+    public static void insertion(List<Integer> list) {
+
+    }
+
+    /** Implementierung des Sortierverfahrens SelectionSort. */
+    public static void selection(List<Integer> list) {
+
+    }
+
+    /** Implementierung des Sortierverfahrens BubbleSort. */
+    public static void bubble(List<Integer> list) {
+
+    }
+
+    /** Implementierung des Sortierverfahrens QuickSort. */
+    public static void quick(List<Integer> list) {
+
+    }
+
+    /** Implementierung des Sortierverfahrens MergeSort. */
+    public static void merge(List<Integer> list) {
+
+    }
+}
